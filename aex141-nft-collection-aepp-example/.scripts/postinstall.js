c9f86c3c899d60e9a084a9eb2fddec261f839c30
Philipp Piwo <github@piwo.space>
2022-08-26 17:01:11 +0200

    add basic frontend

---
+++ aex141-nft-collection-aepp-example/.scripts/postinstall.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+fs.mkdirSync(__dirname + "/../src/assets/contracts/core/", { recursive: true });
+
+const MintableMutableNFT = fs.readFileSync(
+  __dirname + "/../../contracts/MintableMutableNFT.aes",
+  "utf-8"
+);
+fs.writeFileSync(
+  __dirname + "/../src/assets/contracts/MintableMutableNFT.aes.js",
+  `module.exports = \`\n${MintableMutableNFT.replace(/`/g, "\\`")}\`;\n`,
+  "utf-8"
+);
+
+const coreUtils = fs.readFileSync(
+  __dirname + "/../../contracts/core/utils.aes",
+  "utf-8"
+);
+fs.writeFileSync(
+  __dirname + "/../src/assets/contracts/core/utils.aes.js",
+  `module.exports = \`\n${coreUtils.replace(/`/g, "\\`")}\`;\n`,
+  "utf-8"
+);
+
+const coreIAEX141NFTReceiver = fs.readFileSync(
+  __dirname + "/../../contracts/core/IAEX141NFTReceiver.aes",
+  "utf-8"
+);
+fs.writeFileSync(
+  __dirname + "/../src/assets/contracts/core/IAEX141NFTReceiver.aes.js",
+  `module.exports = \`\n${coreIAEX141NFTReceiver.replace(/`/g, "\\`")}\`;\n`,
+  "utf-8"
+);
